11c8320f5e3290127aa126511f84945a683a9626
Dapson55 <andersondedzo@gmail.com>
2022-08-18 07:57:17 +0100

    completed backeng for one

--- onlinebookstore/src/main/java/com/example/REST/LauncherClass.java
+++ onlinebookstore/src/main/java/com/example/REST/LauncherClass.java
@@ -1,15 +1,17 @@
-package in.bushansirgur.onlinebookstore;
+package com.example.REST;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "in.bushansirgur.onlinebookstore.repository")
+
+@EnableJpaRepositories(basePackages = "com.example.REST.repository")
 @SpringBootApplication
-public class OnlinebookstoreApplication {
+public class LauncherClass {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlinebookstoreApplication.class, args);
+		SpringApplication.run(LauncherClass.class, args);
+
 	}
 
 }
